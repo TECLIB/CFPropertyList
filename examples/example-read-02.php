2a4ebeafbf65262780dd1fad71ef1d115d808e6a
Thierry Bugier <tbugier@teclib.com>
2018-04-18 15:07:46 +0200

    docs: update source headers
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- examples/example-read-02.php
+++ examples/example-read-02.php
@@ -1,5 +1,46 @@
 <?php
 /**
+ * LICENSE
+ *
+ * This file is part of CFPropertyList.
+ *
+ * The PHP implementation of Apple's PropertyList can handle XML PropertyLists
+ * as well as binary PropertyLists. It offers functionality to easily convert
+ * data between worlds, e.g. recalculating timestamps from unix epoch to apple
+ * epoch and vice versa. A feature to automagically create (guess) the plist
+ * structure from a normal PHP data structure will help you dump your data to
+ * plist in no time.
+ *
+ * Copyright (c) 2018 Teclib'
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * ------------------------------------------------------------------------------
+ * @author    Christian Kruse <cjk@wwwtech.de>
+ * @copyright Copyright © 2018 Teclib
+ * @package   plist
+ * @license   MIT
+ * @link      https://github.com/TECLIB/CFPropertyList/
+ * ------------------------------------------------------------------------------
+ */
+
+ /**
  * Examples for how to use CFPropertyList
  * Read a Binary PropertyList
  * @package plist
--- src/CFPropertyList/CFTypeDetector.php
+++ src/CFPropertyList/CFTypeDetector.php
@@ -1,17 +1,46 @@
 <?php
 /**
- *Base-Class of all CFTypes used by CFPropertyList.
- * {@link http://developer.apple.com/documentation/Darwin/Reference/ManPages/man5/plist.5.html Property Lists}
- * @author Rodney Rehm <rodney.rehm@medialize.de>
- * @author Christian Kruse <cjk@wwwtech.de>
- * @package plist
- * @subpackage plist.types
- * @version $Id$
- * @example example-create-01.php Using the CFPropertyList API
- * @example example-create-02.php Using CFPropertyList::guess()
- * @example example-create-03.php Using CFPropertyList::guess() with {@link CFDate} and {@link CFData}
+ * LICENSE
+ *
+ * This file is part of CFPropertyList.
+ *
+ * The PHP implementation of Apple's PropertyList can handle XML PropertyLists
+ * as well as binary PropertyLists. It offers functionality to easily convert
+ * data between worlds, e.g. recalculating timestamps from unix epoch to apple
+ * epoch and vice versa. A feature to automagically create (guess) the plist
+ * structure from a normal PHP data structure will help you dump your data to
+ * plist in no time.
+ *
+ * Copyright (c) 2018 Teclib'
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * ------------------------------------------------------------------------------
+ * @author    Rodney Rehm <rodney.rehm@medialize.de>
+ * @author    Christian Kruse <cjk@wwwtech.de>
+ * @copyright Copyright © 2018 Teclib
+ * @package   plist
+ * @license   MIT
+ * @link      https://github.com/TECLIB/CFPropertyList/
+ * @link      http://developer.apple.com/documentation/Darwin/Reference/ManPages/man5/plist.5.html Property Lists
+ * ------------------------------------------------------------------------------
  */
-
 
 namespace CFPropertyList;
 use \DateTime, \Iterator;
@@ -23,14 +52,14 @@ use \DateTime, \Iterator;
   * @author Christian Kruse <cjk@wwwtech.de>
   * @package plist
   * @subpackage plist.types
-  * @example example-create-02.php Using {@link CFTypeDetector}
-  * @example example-create-03.php Using {@link CFTypeDetector} with {@link CFDate} and {@link CFData}
-  * @example example-create-04.php Using and extended {@link CFTypeDetector}
+  * @example example-create-02.php Using CFTypeDetector
+  * @example example-create-03.php Using CFTypeDetector with CFDate and CFData
+  * @example example-create-04.php Using and extended CFTypeDetector
   */
   class CFTypeDetector {
 
   /**
-   * flag stating if all arrays should automatically be converted to {@link CFDictionary}
+   * flag stating if all arrays should automatically be converted to CFDictionary
    * @var boolean
    */
   protected $autoDictionary = false;
@@ -96,7 +125,7 @@ use \DateTime, \Iterator;
 
   /**
    * Create CFType-structure by guessing the data-types.
-   * {@link CFArray}, {@link CFDictionary}, {@link CFBoolean}, {@link CFNumber} and {@link CFString} can be created, {@link CFDate} and {@link CFData} cannot.
+   * CFArray, {@link CFDictionary}, {@link CFBoolean}, {@link CFNumber} and {@link CFString} can be created, {@link CFDate} and {@link CFData} cannot.
    * <br /><b>Note:</b>Distinguishing between {@link CFArray} and {@link CFDictionary} is done by examining the keys.
    * Keys must be strictly incrementing integers to evaluate to a {@link CFArray}.
    * Since PHP does not offer a function to test for associative arrays,
