12331c084ecfb70617c0a2e4f7f762151e20b937
mondrake <mondrake.org@gmail.com>
2019-11-24 18:02:22 +0100

    Update CFBinaryPropertyList.php

--- src/CFPropertyList/CFBinaryPropertyList.php
+++ src/CFPropertyList/CFBinaryPropertyList.php
@@ -940,6 +940,7 @@ abstract class CFBinaryPropertyList
    */
     protected static function binaryStrlen($val)
     {
+        $val = (string) $val;
         for ($i=0; $i<strlen($val); ++$i) {
             if (ord($val[$i]) >= 128) {
                 $val = self::convertCharset($val, 'UTF-8', 'UTF-16BE');
