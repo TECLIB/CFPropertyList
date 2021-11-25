e33edbd46d3fc31679f03a6701b4e7dc365e15a5
Kai Sassnowski <ksassnowski@users.noreply.github.com>
2021-11-25 15:28:43 +0100

    Update method signatures to remove deprecation warning in PHP 8.1
    Signed-off-by: Kai Sassnowski <ksassnowski@users.noreply.github.com>

--- src/CFPropertyList/CFPropertyList.php
+++ src/CFPropertyList/CFPropertyList.php
@@ -667,7 +667,7 @@ class CFPropertyList extends CFBinaryPropertyList implements Iterator
    * @uses $iteratorPosition set to 0
    * @uses $iteratorKeys store keys of {@link $value}
    */
-    public function rewind()
+    public function rewind(): void
     {
         $this->iteratorPosition = 0;
         $this->iteratorKeys = array_keys($this->value);
@@ -676,10 +676,11 @@ class CFPropertyList extends CFBinaryPropertyList implements Iterator
   /**
    * Get Iterator's current {@link CFType} identified by {@link $iteratorPosition}
    * @link http://php.net/manual/en/iterator.current.php
-   * @return CFType current Item
+   * @return mixed current Item
    * @uses $iteratorPosition identify current key
    * @uses $iteratorKeys identify current value
    */
+    #[\ReturnTypeWillChange]
     public function current()
     {
         return $this->value[$this->iteratorKeys[$this->iteratorPosition]];
@@ -688,10 +689,11 @@ class CFPropertyList extends CFBinaryPropertyList implements Iterator
   /**
    * Get Iterator's current key identified by {@link $iteratorPosition}
    * @link http://php.net/manual/en/iterator.key.php
-   * @return string key of the current Item
+   * @return mixed key of the current Item
    * @uses $iteratorPosition identify current key
    * @uses $iteratorKeys identify current value
    */
+    #[\ReturnTypeWillChange]
     public function key()
     {
         return $this->iteratorKeys[$this->iteratorPosition];
@@ -703,7 +705,7 @@ class CFPropertyList extends CFBinaryPropertyList implements Iterator
    * @return void
    * @uses $iteratorPosition increment by 1
    */
-    public function next()
+    public function next(): void
     {
         $this->iteratorPosition++;
     }
@@ -715,7 +717,7 @@ class CFPropertyList extends CFBinaryPropertyList implements Iterator
    * @uses $iteratorPosition test if within {@link $iteratorKeys}
    * @uses $iteratorPosition test if within {@link $value}
    */
-    public function valid()
+    public function valid(): bool
     {
         return isset($this->iteratorKeys[$this->iteratorPosition]) && isset($this->value[$this->iteratorKeys[$this->iteratorPosition]]);
     }
